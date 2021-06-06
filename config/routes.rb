8be9bec0c573b9ceff07ca16b4c1ef45b4de0d58
Kobi Lee <kobiatlaslee@gmail.com>
2021-06-06 14:14:39 -0400

    Feature/Devise: intial commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  resources :class_lists
   root 'home#index'
   get 'home/about'
+  devise_for :users
+  resources :class_lists
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_05_184715) do
+ActiveRecord::Schema.define(version: 2021_06_05_215150) do
 
   create_table "class_lists", force: :cascade do |t|
     t.string "course"
@@ -27,6 +27,18 @@ ActiveRecord::Schema.define(version: 2021_06_05_184715) do
     t.boolean "in_schedule"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
